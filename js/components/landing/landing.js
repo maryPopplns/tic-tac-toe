9ee1e9a4da45528a5a5cd6c2f0eb5af051bee900
Spencer Knight <spencer.knightusn@gmail.com>
2021-07-08 09:14:33 -0700

    added css for different screen sizes

--- js/components/landing/landing.js
+++ js/components/landing/landing.js
@@ -1,5 +1,16 @@
 function landing() {
-  console.log("landing");
+  const BODY = document.body;
+  const LANDING = document.createElement("div");
+  const TIC = document.createElement("div");
+  const TAC = document.createElement("div");
+  const TOE = document.createElement("div");
+  const TTT = [TIC, TAC, TOE];
+
+  LANDING.setAttribute("id", "landing");
+  TTT.forEach((e) => e.setAttribute("class", "ttt_containers"));
+
+  BODY.prepend(LANDING);
+  TTT.forEach((e) => LANDING.append(e));
 }
 
 export { landing };
