48f8f5f9ee4583e1be6c357f0244789f9698895d
Spencer Knight <spencer.knightusn@gmail.com>
2021-07-09 16:28:27 -0700

    clicks now always change tile color

--- src/components/landing/helpers/animator.js
+++ src/components/landing/helpers/animator.js
@@ -8,7 +8,7 @@ function animator() {
   });
 
   (function ANIMATOR() {
-    let count = 50;
+    let count = 30;
     TILES.map((e) => {
       const ELEMENT = document.querySelector(e);
       const ANIMATION_NAME = ELEMENT.getAnimations()[0].animationName;
@@ -16,41 +16,49 @@ function animator() {
       setTimeout(() => {
         ELEMENT.style.animation = `var(--${ANIMATION_NAME})`;
       }, count);
-      count += 50;
+      count += 30;
     });
   })();
 
   const CLICKED = Array.from(document.querySelectorAll(".colored")).map((e) => {
     e.addEventListener("click", (e) => {
-      const TARGET = document.querySelector(`#${e.target.id}`);
-      const NAMES = ["tic", "tac", "toe"];
-      const RANDOM = NAMES[Math.floor(Math.random() * 3)];
-      TARGET.style.animation = `var(--${RANDOM})`;
+      const TILE = document.querySelector(`#${e.target.id}`);
+      const ANIMATION_NAME = TILE.getAnimations()[0].animationName;
+      if (ANIMATION_NAME === "tic") {
+        TILE.style.animation = "var(--tac)";
+      } else if (ANIMATION_NAME === "tac") {
+        TILE.style.animation = "var(--toe)";
+      } else if (ANIMATION_NAME === "toe") {
+        TILE.style.animation = "var(--tic)";
+      }
     });
   });
 
   setTimeout(() => {
     const FLICKER = setInterval(() => {
-      const SET = unique(15);
+      const SET = unique(20);
       for (let i = 0; i < SET.length; i++) {
         const RANDOM_ID = TILES[SET[i]];
         // <-->
         const TILE = document.querySelector(RANDOM_ID);
         const ANIMATION_NAME = TILE.getAnimations()[0].animationName;
+        // console.log(ANIMATION_NAME);
         if (ANIMATION_NAME === "tic") {
           TILE.style.animation = "var(--tac)";
         } else if (ANIMATION_NAME === "tac") {
           TILE.style.animation = "var(--toe)";
-        } else {
+        } else if (ANIMATION_NAME === "toe") {
           TILE.style.animation = "var(--tic)";
         }
-        setTimeout(() => {
-          TILE.style.animation = `var(--${ANIMATION_NAME})`;
-        }, 800);
+
+        // <-no perm change on tiles->
+        // setTimeout(() => {
+        //   TILE.style.animation = `var(--${ANIMATION_NAME})`;
+        // }, 800);
       }
       // <-->
-    }, 2000);
-  }, 4500);
+    }, 1500);
+  }, 2000);
 }
 
 export { animator };
